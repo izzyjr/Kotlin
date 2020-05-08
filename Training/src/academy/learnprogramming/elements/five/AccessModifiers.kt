1496dc886a9e643c314d2e6c3db3492ada41504e
Israel Mesa <mesa.israel50@gmail.com>
2020-05-07 22:06:30 -0400

    Constants and Data Classes

--- Training/src/academy/learnprogramming/elements/five/AccessModifiers.kt
+++ Training/src/academy/learnprogramming/elements/five/AccessModifiers.kt
@@ -1,15 +1,37 @@
 package academy.learnprogramming.elements.five
 
+val MY_CONSTANT = 100
+
 fun main() {
+
+    println(MY_CONSTANT)
 
     val emp = Employee("John")
     println(emp)
 
-    val emp2 = Employee("Joe", false)
+    val emp2 = Employee("John")
     println(emp2)
 
     emp2.fullTime = true
     println(emp2.fullTime)
+
+    println("Hello: ${emp.equals(emp2)}")
+
+    val car = Car("Red", "Ferrari", 1982)
+    println(car)
+
+    val car2 = Car("Blue", "Ferrari", 1982)
+    println("Hello2: ${car == car2}")
+
+    val car3 = car.copy()
+    println(car3)
+
+    val car4 = car.copy("Green", "Renault")
+    println(car4)
+
+}
+
+data class Car(val color: String, val model: String, val year: Int) {
 
 }
 
