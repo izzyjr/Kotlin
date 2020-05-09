bc4ffcc53cf9dbcac3adbf2f746e5d1c834031c1
Israel Mesa <mesa.israel50@gmail.com>
2020-05-08 23:39:25 -0400

    Data Class Practice

--- Training/src/academy/learnprogramming/elements/five/AccessModifiers.kt
+++ Training/src/academy/learnprogramming/elements/five/AccessModifiers.kt
@@ -29,7 +29,29 @@ fun main() {
     val car4 = car.copy("Green", "Renault")
     println(car4)
 
+    val house1 = House("blue", "Mansion", 100)
+    println(house1)
+    val house2 = House("green", "apartment", 50)
+    println(house2)
+    val house3 = house2.copy(color = "purple" ,price = 70)
+    println("This is house3 = $house3")
+    house2.color = "blue"
+    house2.type = "Mansion"
+    house2.price = 100
+    println(house2.toString())
+    println(house1 == house2)
+    println(house1 === house2)
+    println(house1.equals(house2))
+
+    println(house3.hashCode())
+
+
 }
+
+data class House(var color: String, var type: String, var price: Int) {
+
+}
+
 
 data class Car(val color: String, val model: String, val year: Int) {
 
