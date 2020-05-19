c849261095d6615a14ed09e56b90463a5cd1014a
Israel Mesa <mesa.israel50@gmail.com>
2020-05-18 23:28:50 -0400

    The For Loop pt1

---
+++ SectionSix/src/academy/learnprogramming/loops/Loops.kt
@@ -0,0 +1,48 @@
+package academy.learnprogramming.loops
+
+fun main() {
+
+    // Is the number greater than or equal to the start value?
+    // Is the number less than or equal to the end value?
+    val range = 1..5
+    val charRange = 'a'..'z'
+    val stringRange = "ABC".."XYZ"
+
+    println(3 in range)
+    println('q' in charRange)
+    println("CCCCCC" in stringRange)
+
+    val str = "Hello"
+    for (c in str) {
+        println(c)
+    }
+
+    // backward ranges
+    val backwardRange = 5.downTo(1)
+    println(2 in backwardRange)
+
+    // step determined ranges
+    val stepRange = 3..15
+    val stepThree = stepRange.step(3).reversed()
+    val reversedRange = stepRange.reversed()
+    for (i in stepRange) {
+        println(i)
+    }
+
+    for (num in 1..20 step 4) {
+        println(num)
+    }
+
+    for (i in 20 downTo 15) {
+        println(i)
+    }
+
+    for (i in 1 until 10) {
+        println(i)
+    }
+
+    val s = "goodbye"
+    val sRange = 0..s.length
+
+
+}
