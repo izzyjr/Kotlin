1cd0cdca2cf281369ef5bade0df5d904703d1903
Israel Mesa <mesa.israel50@gmail.com>
2020-06-12 22:33:22 -0400

    first commit

---
+++ KotlinAlgorithmsTesting/src/main/kotlincode/Calculator.kt
@@ -0,0 +1,13 @@
+package kotlincode
+
+class Calculator {
+
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun multiply(a: Int, b: Int): Int {
+        return a * b
+    }
+
+}
