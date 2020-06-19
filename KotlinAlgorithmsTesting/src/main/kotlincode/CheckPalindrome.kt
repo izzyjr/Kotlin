602c749b18024cc1fd62589a4c77f1bf7bb417fb
Israel Mesa <mesa.israel50@gmail.com>
2020-06-18 22:49:16 -0400

    first commit

---
+++ KotlinAlgorithmsTesting/src/main/kotlincode/CheckPalindrome.kt
@@ -0,0 +1,14 @@
+package kotlincode
+
+class CheckPalindrome {
+
+    // Given the string, check if it is a palindrome.
+
+    fun checkPalindrome(inputString: String): Boolean {
+
+
+
+        return true
+    }
+
+}
