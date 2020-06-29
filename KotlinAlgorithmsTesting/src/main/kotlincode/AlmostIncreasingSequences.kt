03142dabb930cbf20b9f2cc94f723771665bd2f3
Israel Mesa <mesa.israel50@gmail.com>
2020-06-28 22:30:46 -0400

    AlmostIncreasingSequences pt 2

--- KotlinAlgorithmsTesting/src/main/kotlincode/AlmostIncreasingSequences.kt
+++ KotlinAlgorithmsTesting/src/main/kotlincode/AlmostIncreasingSequences.kt
@@ -5,16 +5,22 @@ class AlmostIncreasingSequences {
     fun almostIncreasingSequence(sequence: MutableList<Int>): Boolean {
         var count: Int = 0
         val max: Int = 2
+        var standard: Int = Int.MIN_VALUE
         for (i in 0 until sequence.size - 1) {
+            standard = sequence[i + 1]
+            println("${sequence[i]} - ${sequence[i + 1]}")
+            println("Standard: $standard")
             if (sequence[i] >= sequence[i + 1]) {
-                println(count)
+                if (sequence[i + 1] <= standard) {
+                    count++
+                }
                 count++
+                println(count)
                 if (count == max) {
                     return false
                 }
             }
         }
-        println(count)
         return true
     }
 
