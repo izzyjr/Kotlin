455616b498cbb19a7e764756764a005e9541634e
Israel Mesa <mesa.israel50@gmail.com>
2020-06-30 22:40:34 -0400

    AlmostIncreasingSequences pt 4

--- KotlinAlgorithmsTesting/src/main/kotlincode/AlmostIncreasingSequences.kt
+++ KotlinAlgorithmsTesting/src/main/kotlincode/AlmostIncreasingSequences.kt
@@ -5,21 +5,33 @@ class AlmostIncreasingSequences {
     fun almostIncreasingSequence(sequence: MutableList<Int>): Boolean {
         var count: Int = 0
         var total: Int = 2
-        var max: Int? = sequence.max()
-        println(max)
-        var min: Int? = sequence.min()
-        println(min)
+        var max: Int = sequence[0]
+        var min: Int = sequence[0]
         for (i in 0 until sequence.size - 1) {
-            if (sequence[i] >= sequence[i + 1]) {
-                if (sequence[i + 1] <= min!!) {
-                    count++
-                }
+            var current: Int = sequence[i]
+            var next: Int = sequence[i + 1]
+            if (current >= next) {
                 count++
-                if (count == total) {
+                if (min <= next) {
+                    count++
+                } else if (min >= next) {
+                    min = next
+                }
+                if (count >= total) {
+                    return false
+                }
+            } else {
+                if (max >= next) {
+                    count++
+                } else if (max <= next) {
+                    max = next
+                }
+                if (count >= total) {
                     return false
                 }
             }
         }
+        println(count)
         return true
     }
 
--- KotlinAlgorithmsTesting/src/test/kotlincode/TestAlmostIncreasingSequences.kt
+++ KotlinAlgorithmsTesting/src/test/kotlincode/TestAlmostIncreasingSequences.kt
@@ -9,7 +9,7 @@ class TestAlmostIncreasingSequences {
 
     @Test
     fun testAlmostIncreasingSequences() {
-        val input: MutableList<Int> = mutableListOf(1, 2, 3, 4, 5, 3, 5, 6)
+        val input: MutableList<Int> = mutableListOf(1, 3, 2, 1)
         val actualResult: Boolean = algorithm.almostIncreasingSequence(input)
         Assert.assertEquals(actualResult, false)
     }
