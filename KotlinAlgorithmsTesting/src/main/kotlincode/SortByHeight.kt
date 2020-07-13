20c24e1eb04d03a37e2f33e20e724594928eb3c4
Israel Mesa <mesa.israel50@gmail.com>
2020-07-12 22:23:19 -0400

    SortByHeight pt2

--- KotlinAlgorithmsTesting/src/main/kotlincode/SortByHeight.kt
+++ KotlinAlgorithmsTesting/src/main/kotlincode/SortByHeight.kt
@@ -3,15 +3,23 @@ package kotlincode
 class SortByHeight {
 
     fun sortByHeight(a: MutableList<Int>): MutableList<Int> {
-        var tallest: Int = Int.MIN_VALUE
+        var empty: Int = 0
         for (i in 0 until a.lastIndex) {
             for (j in 0 until a.lastIndex) {
-                if (a[i] != a[j]) {
-
+                if (j + 1 < a.lastIndex) {
+                    if (a[j] > a[j + 1]) {
+                        println("a[j] = ${a[j]}")
+                        empty = a[j]
+                        a[j] = a[j + 1]
+                        a[j + 1] = empty
+                        println("a[j] = ${a[j]}")
+                    }
                 }
             }
         }
-
+        for (num in a) {
+            print("$num, ")
+        }
         return a
     }
 
