2733a7ed572d51ce3b6fec941ce97531572126f8
Israel Mesa <mesa.israel50@gmail.com>
2020-07-15 22:54:57 -0400

    SortByHeight pt4

--- KotlinAlgorithmsTesting/src/main/kotlincode/SortByHeight.kt
+++ KotlinAlgorithmsTesting/src/main/kotlincode/SortByHeight.kt
@@ -3,35 +3,22 @@ package kotlincode
 class SortByHeight {
 
     fun sortByHeight(a: MutableList<Int>): MutableList<Int> {
-        var empty: Int = 0
-        for (i in 0 until a.lastIndex) {
-            for (j in 0 until a.lastIndex) {
-                if (j + 1 < a.lastIndex) {
-                    if (a[j] != -1) {
-                        if (a[j + 1] == -1 && a[j + 1] != a.lastIndex) {
-                            if (a[j] > a[j + 2]) {
-                                println("a[j] = ${a[j]}")
-                                empty = a[j]
-                                a[j] = a[j + 2]
-                                a[j + 2] = empty
-                                println("a[j] = ${a[j]}")
-                            }
-                        } else  {
-                            if (a[j] > a[j + 2]) {
-                                println("a[j] = ${a[j]}")
-                                empty = a[j]
-                                a[j] = a[j + 2]
-                                a[j + 2] = empty
-                                println("a[j] = ${a[j]}")
-                            }
-                        }
-                    }
-                }
+        var count: Int = 0
+        val itr = a.iterator()
+        val hashMap: HashMap<Int, Int> = hashMapOf()
+
+
+        while (itr.hasNext()) {
+            val t = itr.next()
+            if (t == -1) {
+                hashMap.put(count, t)
+                itr.remove()
             }
+            count++
         }
-        for (num in a) {
-            print("$num, ")
-        }
+
+        hashMap.map { println(it) }
+
         return a
     }
 
